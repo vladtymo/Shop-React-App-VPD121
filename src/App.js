d18836bd8f86e3981963b20034d7bd6c9c736db8
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-05-14 17:38:37 +0300

    Configure routing.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Counter } from './components/Counter';
 import { ProductList } from './components/ProductList';
 import { UserCard } from './components/UserCard';
 import { UserList } from './components/UserList';
 import { USERS } from './components/users';
+import { Menu } from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Hello React!</h1>
-      </header>
+      <BrowserRouter>
+        <header>
+          <h1>Our Logo!</h1>
+          <Menu />
+        </header>
 
-      <UserList users={USERS} />
-      <hr />
-      <ProductList />
-      <hr />
-      <Counter />
+        <Routes>
+          <Route path="/" element={<h1>Main Page</h1>} />
+          <Route path="users" element={<UserList users={USERS} />} />
+          <Route path="counter" element={<Counter />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="*" element={<h1>Page Not Found!</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/users">Users</Link>
+                </li>
+                <li>
+                    <Link to="/products">Products</Link>
+                </li>
+                <li>
+                    <Link to="/counter">Counter</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
